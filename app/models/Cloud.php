3e86b23e07c617bdab288c1206f6550f2f6a7a46
Aaron Grider <aaron.grider@gmail.com>
2015-05-07 13:12:55 -0700

    JSON Fixes

--- app/models/Cloud.php
+++ app/models/Cloud.php
@@ -4,28 +4,28 @@ namespace app\models;
 
 class Cloud extends \lithium\data\Model {
 
-    public static function createCloud($data) {
+    public static function createCloud($json) {
 
         // Create cloud
         $cloud = Cloud::create();
-        $cloud->width = $data->layout->width;
-        $cloud->height = $data->layout->height;
+        $cloud->width = $json["layout"]["width"];
+        $cloud->height = $json["layout"]["height"];
         $cloud->created = date('Y-m-d H:i:s');
         $cloud->save();
 
         // Create words
-        foreach($data->words as $word_data)
+        foreach($json["words"] as $word_data)
         {
             $word = Word::create();
             $word->cloudid = $cloud->id;
-            $word->name = $word_data->name;
-            $word->count = $word_data->count;
+            $word->name = $word_data["name"];
+            $word->count = $word_data["count"];
 
             // Bounds
-            $word->bottom = $word_data->bounds->bottom;
-            $word->top = $word_data->bounds->top;
-            $word->left = $word_data->bounds->left;
-            $word->right = $word_data->bounds->right;
+            $word->bottom = $word_data["bounds"]["bottom"];
+            $word->top = $word_data["bounds"]["top"];
+            $word->left = $word_data["bounds"]["left"];
+            $word->right = $word_data["bounds"]["right"];
 
             $word->save();
         }
