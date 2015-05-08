642ab2c4baa8f9cf50cdb72fe2fd07428587bcfd
Aaron Grider <aaron.grider@gmail.com>
2015-05-07 22:30:43 -0700

    Word cloud fixes

--- app/views/cloud/wordCloud.html.php
+++ app/views/cloud/wordCloud.html.php
@@ -1,17 +1,24 @@
-<div style="width: <?=$cloud->width;?>; height: <?=$cloud->height;?>">
+<div style="width: <?=$cloud->width;?>; height: <?=$cloud->height; ?> overflow: scroll">
 
     <?php foreach($words as $word) {
 
+        // If we have an unpositioned word
+        if ($word->top == 0 & $word->left == 0)
+        {
+            return;
+        }
+        else {
         $width = $word->right - $word->left;
         $height = $word->bottom - $word->top;
-
     ?>
 
             <div style="position: absolute; width: <?=$width;?>; height: <?=$height;?>; top: <?=$word->top;?>; left: <?=$word->left;?>;">
                 <button class="word btn waves-effect waves-light blue" type="" name="action"><?=$word->name;?></button>
             </div>
 
-    <?php } ?>
+    <?php }
+
+    }?>
 
 </div>
 
