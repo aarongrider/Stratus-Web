26b856d330727ee226ec75e60f5b870e4e197578
Aaron Grider <aaron.grider@gmail.com>
2015-05-08 10:59:49 -0700

    Handle null

--- app/controllers/CloudController.php
+++ app/controllers/CloudController.php
@@ -77,6 +77,10 @@ class CloudController extends \lithium\action\Controller
             $json = $this->request->data;
             $cloudid = $json["id"];
             $cloud = Cloud::find('first', array('conditions' => array('id' => $cloudid)));
+
+            // If we could not fine a cloud
+            if ($cloud == null) $cloud = "none";
+
             return $cloud;
         }
     }
