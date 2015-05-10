6207324d7694dfd5d209243014c40ea5b9c13f3c
Aaron Grider <aaron.grider@gmail.com>
2015-05-10 16:15:39 -0700

    Refactor api calls

---
+++ app/controllers/APIController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Cloud;
+use app\models\Words;
+
+class APIController extends \lithium\action\Controller
+{
+
+    public function save()
+    {
+        // Take in JSON post
+        if ($this->request->data) // If we have submitted the form
+        {
+            $cloud = Cloud::createCloud($this->request->data);
+            return $cloud->id;
+        }
+    }
+
+    public function load()
+    {
+        // Take in JSON post
+        if ($this->request->data) // If we have submitted the form
+        {
+            $json = $this->request->data;
+            $cloudid = $json["id"];
+            $cloud = Cloud::find('first', array('conditions' => array('id' => $cloudid), 'with' => array('Words')));
+
+            // If we could not fine a cloud
+            if ($cloud == null) $cloud = "none";
+
+            return compact('cloud');
+        }
+    }
+
+}
+
+?>
--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -34,32 +34,6 @@ class MainController extends \lithium\action\Controller
         return compact('cloudid');
     }
 
-    public function save()
-    {
-        // Take in JSON post
-        if ($this->request->data) // If we have submitted the form
-        {
-            $cloud = Cloud::createCloud($this->request->data);
-            return $cloud->id;
-        }
-    }
-
-    public function load()
-    {
-        // Take in JSON post
-        if ($this->request->data) // If we have submitted the form
-        {
-            $json = $this->request->data;
-            $cloudid = $json["id"];
-            $cloud = Cloud::find('first', array('conditions' => array('id' => $cloudid), 'with' => array('Words')));
-
-            // If we could not fine a cloud
-            if ($cloud == null) $cloud = "none";
-
-            return compact('cloud');
-        }
-    }
-
 }
 
 ?>
