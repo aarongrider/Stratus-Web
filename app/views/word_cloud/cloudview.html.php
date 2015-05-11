9b7f4c833195bbf7126c2d6c5a253db8df6cd283
Aaron Grider <aaron.grider@gmail.com>
2015-05-11 00:44:49 -0700

    Changed time stamp source

--- app/views/word_cloud/cloudview.html.php
+++ app/views/word_cloud/cloudview.html.php
@@ -12,6 +12,7 @@
         <a href="/WordCloud/listview/<?=$cloud->id;?>" class="waves-effect waves-light btn green">View Word List</a> <br>
         <h5 class="grey-text" style="margin-top: 30px;">CLOUDID</h5>
         <h1 style="margin-top: 0px;"><?=$cloud->id;?></h1>
+        <p>Created <?=date("F j, Y, g:i a", strtotime($cloud->timestamp));?></p>
 
         <!-- Share Dropdown -->
         <ul id='dropdown-share' class='dropdown-content' style="z-index: 1; margin-top: 50px;">
@@ -19,7 +20,6 @@
             <li><a target="_blank" href="https://twitter.com/home?status=I just made a word cloud! http://voicecloudapp.com/WordCloud/cloudview/<?=$cloud->id;?>">Twitter</a></li>
         </ul>
 
-        <p>Created <?=date("F j, Y, g:i a", strtotime($cloud->created));?></p>
     </div>
 
     <?php foreach($words as $word) {
